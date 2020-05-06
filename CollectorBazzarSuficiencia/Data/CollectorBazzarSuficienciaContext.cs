999f17522d9acfefbb438c2198b7d874ea8d5ca3
kristati <ksegurag@ucenfotec.ac.cr>
2020-05-06 01:41:06 -0600

    primeras vistas y controladores del lado del cliente

---
+++ CollectorBazzarSuficiencia/Data/CollectorBazzarSuficienciaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CollectorSuficiencia.Entities;
+
+namespace CollectorBazzarSuficiencia.Data
+{
+    public class CollectorBazzarSuficienciaContext : DbContext
+    {
+        public CollectorBazzarSuficienciaContext (DbContextOptions<CollectorBazzarSuficienciaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CollectorSuficiencia.Entities.Usuario> Usuario { get; set; }
+
+        public DbSet<CollectorSuficiencia.Entities.Producto> Producto { get; set; }
+    }
+}
